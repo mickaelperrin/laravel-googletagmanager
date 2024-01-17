780dc622cf85406aa47e3486c67e5bfe325784a3
Mickaël Perrin <contact@mickaelperrin.fr>
2024-01-17 18:44:03 +0100

    👌 IMPROVE: Send gtm data asap
    Message event should be triggered before sirdata
    
    Signed-off-by: Mickaël Perrin <contact@mickaelperrin.fr>

--- resources/views/body.blade.php
+++ resources/views/body.blade.php
@@ -1,15 +1,4 @@
 @if($enabled)
-    <script>
-        function gtmPush() {
-            @unless(empty($dataLayer->toArray()))
-            window.dataLayer.push({!! $dataLayer->toJson() !!});
-            @endunless
-            @foreach($pushData as $item)
-            window.dataLayer.push({!! $item->toJson() !!});
-            @endforeach
-        }
-        addEventListener("load", gtmPush);
-    </script>
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ $id }}"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 @endif
--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -7,4 +7,17 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','{{ $id }}');</script>
+<script>
+    function gtmPush() {
+        @unless(empty($dataLayer->toArray()))
+        window.dataLayer.push({!! $dataLayer->toJson() !!});
+        @endunless
+        @foreach($pushData as $item)
+        window.dataLayer.push({!! $item->toJson() !!});
+        @endforeach
+    }
+    gtmPush();
+</script>
 @endif
+
+
